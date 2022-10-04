47842d5eba51396a25cad7d37ab0b246f4bc35ab
Juan Santana Salgado <jsantanas@unal.edu.co>
2022-10-04 18:51:20 -0500

    Add compile, fit and callbacks
    Callbacks for early stopping and learning rate reduction will be updated to monitor the IoU metric, which is for this case the appropriate metric for evaluating the net's performance.

--- Deep Learning/Capillaroscopy project/u_net.py
+++ Deep Learning/Capillaroscopy project/u_net.py
@@ -48,3 +48,30 @@ out_layer = Conv2D(2,(1,1),padding='valid')(x)
 unet = Model(inputs=in_layer,outputs=out_layer)
 
 unet.summary()
+
+# Model compilation
+unet.compile(optimizer=opts.Adam(learning_rate=0.0001),loss='binary_crossentropy',metrics=['accuracy','mae'])
+
+# Callbacks definition
+class TrainingCallback(Callback):
+  def on_train_begin(self,logs=None):
+    print('Starting training...')
+  def on_epoch_begin(self,epoch,logs=None):
+    print(f"Starting epoch {epoch+1}")
+  def on_epoch_end(self,epoch,logs=None):
+    print(f"Finished epoch {epoch+1}")
+  def on_train_end(self,logs=None):
+    print('Finished training')
+
+checkpoint_path = '/content/gdrive/MyDrive/Semillero/checkpoint'
+checkpoint = ModelCheckpoint(checkpoint_path,frequency='epoch',
+save_weights_only=1,save_best_only=1,verbose=1,monitor='val_accuracy')
+
+early_stopping = EarlyStopping(monitor='val_accuracy',min_delta=1e-3,patience=10)
+reduce_lr = ReduceLROnPlateau(monitor='val_accuracy',factor=0.5,patience=2,\
+                              verbose=1,min_delta=1e-3)
+
+# Model training
+history = unet.fit(train_data,train_labels,epochs=50,batch_size=16,
+                    validation_data=(validation_data,validation_labels),verbose=2),
+                    callbacks=[checkpoint,TrainingCallback(),early_stopping,reduce_lr])
